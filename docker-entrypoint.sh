8025f2c333120503856b6a32920f357b5f9009ed
bs01631 <firoz@brainstation-23.com>
2025-11-01 11:31:05 +0600

    try again 7

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+# Create .env if missing
+if [ ! -f .env ]; then
+    cp .env.example .env
+fi
+
 # Generate APP_KEY if missing
 php artisan key:generate --force
 
-# Clear Laravel caches
+# Run migrations
+php artisan migrate --force
+
+# Clear caches
 php artisan config:clear
 php artisan cache:clear
 php artisan view:clear
